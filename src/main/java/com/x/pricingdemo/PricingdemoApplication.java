01aa1b71304a0b5d2b6196cf3dd56beda82402e0
Chamith Karunakalage <chamithk@eteknowledge.com>
2021-02-20 21:44:34 +0530

    pricing demo initialization

---
+++ src/main/java/com/x/pricingdemo/PricingdemoApplication.java
@@ -0,0 +1,13 @@
+package com.x.pricingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricingdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/x/pricingdemo/PricingdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x.pricingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
